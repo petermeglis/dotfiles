fd618a0db18584d80224fb0dc1cebfb752239d1e
Peter Meglis <petermeglis@github.com>
2023-04-30 09:02:13 -0600

    Fix interpolation

--- secrets/aws.zsh
+++ secrets/aws.zsh
@@ -1,3 +1,3 @@
 # Aliases to run cli commands for specific user access
-alias aws-deals="aws --profile $DEALS_BUCKET_USER"
-alias aws-pm="aws --profile $PETERMEGLIS_EC2_USER"
+alias aws-deals="aws --profile ${DEALS_BUCKET_USER}"
+alias aws-pm="aws --profile ${PETERMEGLIS_EC2_USER}"
