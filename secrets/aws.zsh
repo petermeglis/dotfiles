bcae9cef967d1eed1c4f18a970327bdfa00b5fb9
Peter Meglis <petermeglis@github.com>
2023-04-30 09:48:52 -0600

    Move AWS aliases to zsh aliases

--- secrets/aws.zsh
+++
@@ -1,3 +0,0 @@
-# Aliases to run cli commands for specific user access
-alias aws-deals="aws --profile ${DEALS_BUCKET_USER}"
-alias aws-pm="aws --profile ${PETERMEGLIS_EC2_USER}"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,5 @@
 alias reload!='. ~/.zshrc'
+
+# AWS CLI - to run cli commands for specific user access
+alias aws-deals="aws --profile ${DEALS_BUCKET_USER}"
+alias aws-pm="aws --profile ${PETERMEGLIS_EC2_USER}"
