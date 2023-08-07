e318d9dde78b0032487f1bf2267ffde5ed451435
petermeglis <petermeglis@gmail.com>
2023-08-07 13:22:30 +0200

    Change screenshots location

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -10,3 +10,7 @@ defaults write com.apple.finder ShowPathbar -bool true
 
 # Display full POSIX path as Finder window title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Change location for screenshots
+defaults write com.apple.screencapture location ~/fs/media/screenshots
+killall SystemUIServer
